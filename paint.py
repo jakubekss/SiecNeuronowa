eaa564d8cc64df69df0f87264976fe218343c612
Jakub <61504210+jakubekss@users.noreply.github.com>
2020-06-16 16:56:33 +0200

    Update paint.py

--- paint.py
+++ paint.py
@@ -28,16 +28,7 @@ from keras.utils import to_categorical
 from matplotlib.image import imread
 import numpy as np
 
-# Wczytanie modelu z pliku
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-
-loaded_model = model_from_json(loaded_model_json)
-
-# Wczytanie Wag
-loaded_model.load_weights("model.h5")
-
+epoki = 0
 
 # Funkcja przekształcająca zdjęcie kolorowe RGB do skali szarości
 def rgb2gray(rgb):
@@ -160,14 +151,18 @@ class Window(QMainWindow):
         # Przeształcenie zdjęcia do takiej samej stukrtury jak dane uczace, N elementowa lista zdjęć 28x28x1
         gray = gray.reshape((1, 28, 28, 1)) 
 
-        # użycie modelu do przewidzenia jaki jest liczba na obrazie
-        prediction = loaded_model.predict(gray)
+        if epoki == 0:
+            pass
+        
+        else:
+            # użycie modelu do przewidzenia jaki jest liczba na obrazie
+            prediction = loaded_model.predict(gray)
 
-        # wyświetlenie wartości zwróconych przez sieć 
-        print(prediction)
+            # wyświetlenie wartości zwróconych przez sieć 
+            print(prediction)
 
-        # wysiwetlenie indeksu najwiekszego elementu 
-        print(np.argmax(prediction))
+            # wysiwetlenie indeksu najwiekszego elementu 
+            print(np.argmax(prediction))
 
         
     def paintEvent(self, event):
