be8e8b46a096d33fd2750a7895eda5c0f4967d4f
Jakub <61504210+jakubekss@users.noreply.github.com>
2020-06-16 15:09:35 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMenu, QMenuBar, QAction, QFileDialog, QGridLayout, QLineEdit, QWidget, QDockWidget, QLabel
+from PyQt5.QtGui import QImage, QPainter, QPen, QBrush, QPixmap
+from PyQt5.QtCore import Qt, QPoint
+import sys
+import qimage2ndarray 
+import cv2
+
+import numpy as np
+
+import os
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.datasets import mnist 
+from keras import layers 
+from keras import models 
+from keras.utils import to_categorical
+
+from matplotlib.image import imread
+
+
+from paint import Window
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    app.exec()
