fcd4fd9d79048910a52aa24109adbf9215688c8b
Jakub <61504210+jakubekss@users.noreply.github.com>
2020-06-16 15:40:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QLineEdit, QPushButton, QMessageBox
 import sys
 import math
-import network
+#import network
 
 class Aplikacja(QWidget):
     def __init__(self):
@@ -16,12 +16,14 @@ class Aplikacja(QWidget):
         etykieta1 = QLabel("Ilość epok", self)
         etykieta2 = QLabel("Batch size", self)
         etykieta3 = QLabel("Ilość neuronów", self)
+        etykieta4 = QLabel("Wybierz model",self)
         
         # przypisanie widgetów do układu tabeli
         ukladT = QGridLayout()
         ukladT.addWidget(etykieta1, 0, 0)
         ukladT.addWidget(etykieta2, 1, 0)
         ukladT.addWidget(etykieta3, 2, 0)
+        ukladT.addWidget(etykieta4, 4, 0)
         
         # 1-liniowe pola edycyjne
         self.epoki = QLineEdit()
@@ -35,20 +37,24 @@ class Aplikacja(QWidget):
         
 
         # przyciski
+        wybierzBtn = QPushButton("&Wybierz", self)
+        wybierzBtn.clicked.connect()
+
         uczBtn = QPushButton("&Stwórz model", self)
-        uczBtn.clicked.connect(self.tworzenieModelu)
+        #uczBtn.clicked.connect(self.tworzenieModelu)
         uczBtn.resize(uczBtn.sizeHint())
 
         zamknijBtn = QPushButton("&Zamknij", self)
         zamknijBtn.clicked.connect(quit)
 
-        ukladT.addWidget(uczBtn, 6, 0, 1, 2)
-        ukladT.addWidget(zamknijBtn, 7, 1)
+        ukladT.addWidget(zamknijBtn, 4, 1)
+        ukladT.addWidget(uczBtn, 3, 0, 1, 2)
+        ukladT.addWidget(zamknijBtn, 5, 1)
 
         # przypisanie utworzonego układu do okna
         self.setLayout(ukladT)
     
-        self.resize(300, 150)
+        self.setGeometry(928, 400, 300, 150)
         self.setWindowTitle("Tworzenie Modelu")
         self.show()
     
@@ -56,17 +62,17 @@ class Aplikacja(QWidget):
 
         nadawca = self.sender()
 
-        try:
-            x = float(self.epoki.text())
-            y = float(self.batch.text())
-            z = float(self.neurony.text())
+        # try:
+        #     x = float(self.epoki.text())
+        #     y = float(self.batch.text())
+        #     z = float(self.neurony.text())
             
-            if nadawca.text() == "&Stwórz model":
-                network.tworzenieModelu(x, y, z) 
+        #     if nadawca.text() == "&Stwórz model":
+        #         network.tworzenieModelu(x, y, z) 
                      
 
-        except ValueError:
-            QMessageBox.warning(self, "Błąd", "Błędne dane", QMessageBox.Ok)
+        # except ValueError:
+        #     QMessageBox.warning(self, "Błąd", "Błędne dane", QMessageBox.Ok)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
